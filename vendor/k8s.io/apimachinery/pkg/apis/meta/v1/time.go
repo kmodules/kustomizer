ed4628bdfaa1fcd56b47a4947191b0e536367d27
Tamal Saha <tamal@appscode.com>
2021-05-05 11:15:06 -0700

    Use k8s 1.21.0 toolchain
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- vendor/k8s.io/apimachinery/pkg/apis/meta/v1/time.go
+++ vendor/k8s.io/apimachinery/pkg/apis/meta/v1/time.go
@@ -20,7 +20,7 @@ import (
 	"encoding/json"
 	"time"
 
-	fuzz "github.com/google/gofuzz"
+	"github.com/golang/protobuf/jsonpb"
 )
 
 // Time is a wrapper around time.Time which supports correct
@@ -163,6 +163,14 @@ func (t Time) ToUnstructured() interface{} {
 	return string(buf)
 }
 
+func (t Time) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
+	return t.MarshalJSON()
+}
+
+func (t *Time) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, jstr []byte) error {
+	return t.UnmarshalJSON(jstr)
+}
+
 // OpenAPISchemaType is used by the kube-openapi generator when constructing
 // the OpenAPI spec of this type.
 //
@@ -182,16 +190,3 @@ func (t Time) MarshalQueryParameter() (string, error) {
 
 	return t.UTC().Format(time.RFC3339), nil
 }
-
-// Fuzz satisfies fuzz.Interface.
-func (t *Time) Fuzz(c fuzz.Continue) {
-	if t == nil {
-		return
-	}
-	// Allow for about 1000 years of randomness.  Leave off nanoseconds
-	// because JSON doesn't represent them so they can't round-trip
-	// properly.
-	t.Time = time.Unix(c.Rand.Int63n(1000*365*24*60*60), 0)
-}
-
-var _ fuzz.Interface = &Time{}
